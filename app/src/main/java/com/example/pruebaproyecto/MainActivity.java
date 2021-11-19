e5f63f0936d2981dcd974147114abac1d54a1a7d
emiflocas <a18300136@ceti.mx>
2021-11-19 15:06:32 -0600

    LogIn Activity añadida (Incompleta)

--- app/src/main/java/com/example/pruebaproyecto/MainActivity.java
+++ app/src/main/java/com/example/pruebaproyecto/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.pruebaproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email =  findViewById(R.id.editTextEmail_LogIn);
+        password = findViewById(R.id.editTextPassword_LogIn);
+    }
+
+    public void onClickLogIn(View view) {
+        String emailString = email.getText().toString(), passwordString = password.getText().toString();
+    }
+
+    public void onClickRegister(View view) {
+        Intent changeRegister = new Intent(this, RegisterActivity.class);
+        startActivity(changeRegister);
     }
 }
